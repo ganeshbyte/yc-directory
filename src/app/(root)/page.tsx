bd1ab0b0245e36a0984ad482e09edb6d7ab77b22
ganeshbyte <gc930954@gmail.com>
2025-01-16 11:06:14 +0530

    support for tailwind utility classes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,7 @@
+import Banner from "../components/Banner";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <Banner></Banner>;
 };
 
 export default Home;
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { auth, signIn, signOut } from "../../../auth";
 const Navbar = async () => {
   const session = await auth();
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-5 py-3 shadow-sm font-work-sans-400">
       <nav className="flex justify-between items-center">
         <Link href={"/"}>
           <img src="/logo.png" alt="logo" width={100} height={50} />
@@ -19,6 +19,7 @@ const Navbar = async () => {
                 <form
                   action={async () => {
                     "use server";
+
                     await signOut({ redirectTo: "/" });
                   }}
                 >
