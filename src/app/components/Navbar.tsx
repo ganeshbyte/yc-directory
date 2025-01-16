1e9d704cab8247872b2fbaa4185dfc4e202bf5f8
ganeshbyte <gc930954@gmail.com>
2025-01-16 09:52:10 +0530

    support for layout in the root

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,53 @@
-const Navbar = () => {
+import Link from "next/link";
+import { auth, signIn, signOut } from "../../../auth";
+
+const Navbar = async () => {
+  const session = await auth();
   return (
-    <div className="w-full bg-gray-300 flex justify-evenly p-5 text-3xl font-bold">
-      <div>Hello</div>
-      <div>@GaneshChaudhari</div>
-    </div>
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href={"/"}>
+          <img src="/logo.png" alt="logo" width={100} height={50} />
+        </Link>
+
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <div className="flex items-center gap-5">
+                <Link href={"/create"}>Create</Link>
+
+                <form
+                  action={async () => {
+                    "use server";
+                    await signOut({ redirectTo: "/" });
+                  }}
+                >
+                  <button>
+                    <span>signout</span>
+                  </button>
+                </form>
+
+                <Link href={`/user/${session?.user?.id}`}>
+                  <span>{session?.user?.name}</span>
+                </Link>
+              </div>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 };
 
